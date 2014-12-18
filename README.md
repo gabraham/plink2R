3f7fccdde5547c19cf2a9ee7dbf81ece370f56f8
Gad Abraham <abrahamg@unimelb.edu.au>
2014-12-18 23:17:31 +1100

    Cleaned up docs

--- README.md
+++ README.md
@@ -17,5 +17,11 @@ Example:
    dim(dat$bim)
    ```
 
-Also see the file plink2.R (required glmnet and doMC)
+Also see the file plink2.R for an example.
+
+If there are missing genotypes, these will by default be assigned as NA. To do
+simple imputation of missing genotypes (for each SNP, randomly assigning to
+the missing genotypes values sampled proportionally to the non-missing
+genotypes in the SNP), use `read_plink("data", impute="random")`.
+
 
--- plink2R.R
+++ plink2R.R
@@ -1,9 +1,5 @@
 
 library(plink2R)
-library(glmnet)
-library(doMC)
-
-registerDoMC(cores=10)
 
 root <- "data"
 
@@ -16,16 +12,10 @@ sum(r1$bed == 3)
 max(r1$bed)
 
 system.time({
-   r2 <- read_plink(root, impute=FALSE)
+   r2 <- read_plink(root, impute="random")
 })
 dim(r2$bed)
 sum(is.na(r2$bed))
 sum(r2$bed == 3, na.rm=TRUE)
 max(r2$bed, na.rm=TRUE)
-
-g <- cv.glmnet(r1$bed, r1$fam[, 6] - 1, 
-   family="binomial", nfolds=10, type="auc", dfmax=100,
-   parallel=TRUE)
-
-max(g$cvm)
 
